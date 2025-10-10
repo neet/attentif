4dad3de77fd8124560938f4976ba7f418bb6f875
Ryo Igarashi <n33t5hin@gmail.com>
2025-10-10 21:13:24 +0900

    feat: Support passing ndarray in softmax

--- src/softmax.py
+++ src/softmax.py
@@ -1,14 +1,8 @@
 import numpy as np
+import numpy.typing as npt
 
 # https://manabitimes.jp/math/1371
-def softmax(xs):
-  denominator = sum([
-    np.exp(x)
-    for x in xs
-  ])
-
-  return [
-      np.exp(x) / denominator
-      for x in xs
-  ]
-
+def softmax(_xs: npt.ArrayLike) -> np.ndarray:
+    xs = np.asarray(_xs, dtype=float)
+    xs = xs - np.max(xs, axis=-1, keepdims=True)
+    return np.exp(xs) / np.sum(np.exp(xs), axis=-1, keepdims=True)
