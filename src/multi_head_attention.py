d86ec8db5be6033919c338393187b1fc3d6eceb9
Ryo Igarashi <n33t5hin@gmail.com>
2025-11-02 14:03:01 +0900

    Parallelize multi-head attention by view and permute

--- src/multi_head_attention.py
+++ src/multi_head_attention.py
@@ -5,30 +5,28 @@ from typing import Optional
 
 from .softmax import softmax
 
+# B: バッチサイズ
+# S: トークンの最大長
+# h: ヘッドの個数
+# d_*: 1つのヘッドが持つ隠れ状態の次元
 class MultiHeadAttention(nn.Module):
-    # B: バッチサイズ
-    # S: トークンの最大長
-    # h: ヘッドの個数
-    # d_k: 1つのヘッドが持つ隠れ状態の次元
-    # H: モデル全体の隠れ状態の次元 (H = h*d_k)
     def __init__(self, h: int, d_k: int, d_v: int) -> None:
         super().__init__()
 
         self.h = h
         self.d_k = d_k
         self.d_v = d_v
-        self.H = h * d_k
 
-        self.W_Q = nn.Parameter(torch.empty(self.H, h * d_k))
-        self.W_K = nn.Parameter(torch.empty(self.H, h * d_k))
-        self.W_V = nn.Parameter(torch.empty(self.H, h * d_v))
+        self.W_Q = nn.Parameter(torch.empty(h*d_k, h*d_k))
+        self.W_K = nn.Parameter(torch.empty(h*d_k, h*d_k))
+        self.W_V = nn.Parameter(torch.empty(h*d_k, h*d_v))
 
-        self.b_Q = nn.Parameter(torch.zeros(h * d_k))
-        self.b_K = nn.Parameter(torch.zeros(h * d_k))
-        self.b_V = nn.Parameter(torch.zeros(h * d_v))
+        self.b_Q = nn.Parameter(torch.zeros(h*d_k))
+        self.b_K = nn.Parameter(torch.zeros(h*d_k))
+        self.b_V = nn.Parameter(torch.zeros(h*d_v))
 
-        self.W_O = nn.Parameter(torch.empty(h * d_v, self.H))
-        self.b_O = nn.Parameter(torch.zeros(self.H))
+        self.W_O = nn.Parameter(torch.empty(h*d_v, h*d_k))
+        self.b_O = nn.Parameter(torch.zeros(h*d_k))
 
         self._reset_parameters()
 
@@ -36,45 +34,46 @@ class MultiHeadAttention(nn.Module):
         for param in [self.W_Q, self.W_K, self.W_V, self.W_O]:
             nn.init.xavier_uniform_(param)
 
-    # batch: (B, S, H)
-    # attention_mask: (B, S, S)
-    # return: (B, S, H)
+    # f: (B, S, h*d_k) -> (B, S, S) -> (B, S, h*d_v)
     def forward(self, batch: torch.Tensor, attention_mask: Optional[torch.Tensor] = None) -> torch.Tensor:
-        outputs: list[torch.Tensor] = []
+        B, S, _ = batch.shape
 
-        for i in range(0, self.h):
-            # i番目のheadにかける重みをスライス
-            W_Q = self.W_Q[:, i*self.d_k : (i+1)*self.d_k]
-            W_K = self.W_K[:, i*self.d_k : (i+1)*self.d_k]
-            W_V = self.W_V[:, i*self.d_v : (i+1)*self.d_v]
+        # (B, S, h*d)
+        Q = batch @ self.W_Q + self.b_Q
+        K = batch @ self.W_K + self.b_K
+        V = batch @ self.W_V + self.b_V
 
-            b_Q = self.b_Q[i*self.d_k : (i+1)*self.d_k]
-            b_K = self.b_K[i*self.d_k : (i+1)*self.d_k]
-            b_V = self.b_V[i*self.d_v : (i+1)*self.d_v]
+        # (B, S, h, d)
+        Q = Q.view(B, S, self.h, self.d_k)
+        K = K.view(B, S, self.h, self.d_k)
+        V = V.view(B, S, self.h, self.d_v)
 
-            # (B, S, d_k)
-            Q = batch @ W_Q + b_Q
-            # (B, S, d_k)
-            K = batch @ W_K + b_K
-            # (B, S, d_v)
-            V = batch @ W_V + b_V
+        # (B, h, S, d)
+        Q = Q.permute(0, 2, 1, 3).contiguous()
+        K = K.permute(0, 2, 1, 3).contiguous()
+        V = V.permute(0, 2, 1, 3).contiguous()
 
-            # (B, S, S)
-            scores = Q @ K.mT / math.sqrt(self.d_k)
+        # (B, h, S, S)
+        scores = Q @ K.mT / math.sqrt(self.d_k)
 
-            if attention_mask is not None:
-                scores = scores + attention_mask
+        if attention_mask is not None:
+            # (B, h, S, S) + (B, 1, S, S)
+            scores = scores + attention_mask.unsqueeze(1).to(dtype=scores.dtype, device=scores.device)
 
-            # (B, S, S)
-            attention_weights = softmax(scores, dim=-1)
-            # (B, S, d_v)
-            output = attention_weights @ V
+        # (B, h, S, S)
+        attention_weights = softmax(scores, dim=-1)
 
-            outputs.append(output)
+        # (B, h, S, d_v)
+        output = attention_weights @ V
 
-        # (B, S, H)
-        output = torch.cat(outputs, dim=-1)
+        # (B, S, h, d_v)
+        output = output.permute(0, 2, 1, 3).contiguous()
 
-        # (B, S, H)
-        return output @ self.W_O + self.b_O
+        # (B, S, h*d_v)
+        output = output.view(B, S, self.h*self.d_v)
+
+        # (B, S, h*d_v)
+        output = output @ self.W_O + self.b_O
+
+        return output
 
